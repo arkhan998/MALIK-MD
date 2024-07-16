f215aa9379ff922f2fef0aa3d38c6a08836fd44a
adithappu1234 <appuadith39@gmail.com>
2024-07-16 13:09:21 -0300

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -18,7 +18,7 @@ const config = {
   REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || "public",
-  OWNER_NAME: process.env.OWNER_NAME || "ᴀᴅɪᴛʜ🧃",
+  OWNER_NAME: process.env.OWNER_NAME || "ᴇᴛʜɪx xsɪᴅ🧃",
   OWNER_NUMBER: process.env.OWNER_NUMBER || "918075116840",
   GEMINI_KEY: process.env.GEMINI_KEY || "",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
