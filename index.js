672ed0928accb79bf279124f9e2f1277de9c1646
Ethix-Xsid <145905667+Ethix-Xsid@users.noreply.github.com>
2024-08-12 21:30:27 +0530

    update index.js

--- index.js
+++ index.js
@@ -4,19 +4,27 @@ dotenv.config();
 import {
     makeWASocket,
     Browsers,
+    jidDecode,
+    makeInMemoryStore,
+    makeCacheableSignalKeyStore,
     fetchLatestBaileysVersion,
     DisconnectReason,
     useMultiFileAuthState,
+    getAggregateVotesInPollMessage
 } from '@whiskeysockets/baileys';
 import { Handler, Callupdate, GroupUpdate } from './src/event/index.js';
+import { Boom } from '@hapi/boom';
 import express from 'express';
 import pino from 'pino';
 import fs from 'fs';
 import NodeCache from 'node-cache';
 import path from 'path';
 import chalk from 'chalk';
+import { writeFile } from 'fs/promises';
 import moment from 'moment-timezone';
 import axios from 'axios';
+import fetch from 'node-fetch';
+import * as os from 'os';
 import config from './config.cjs';
 import pkg from './lib/autoreact.cjs';
 const { emojis, doReact } = pkg;
@@ -25,7 +33,8 @@ const sessionName = "session";
 const app = express();
 const orange = chalk.bold.hex("#FFA500");
 const lime = chalk.bold.hex("#32CD32");
-let useQR = false;
+let useQR;
+let isSessionPutted;
 let initialConnection = true;
 const PORT = process.env.PORT || 3000;
 
@@ -36,6 +45,13 @@ const logger = MAIN_LOGGER.child({});
 logger.level = "trace";
 
 const msgRetryCounterCache = new NodeCache();
+
+const store = makeInMemoryStore({
+    logger: pino().child({
+        level: 'silent',
+        stream: 'store'
+    })
+});
 
 const __filename = new URL(import.meta.url).pathname;
 const __dirname = path.dirname(__filename);
@@ -50,7 +66,7 @@ if (!fs.existsSync(sessionDir)) {
 async function downloadSessionData() {
     if (!config.SESSION_ID) {
         console.error('Please add your session to SESSION_ID env !!');
-        return false;
+        process.exit(1);
     }
     const sessdata = config.SESSION_ID.split("Ethix-MD&")[1];
     const url = `https://pastebin.com/raw/${sessdata}`;
@@ -59,11 +75,14 @@ async function downloadSessionData() {
         const data = typeof response.data === 'string' ? response.data : JSON.stringify(response.data);
         await fs.promises.writeFile(credsPath, data);
         console.log("🔒 Session Successfully Loaded !!");
-        return true;
     } catch (error) {
-       // console.error('Failed to download session data:', error);
-        return false;
+        console.error('Failed to download session data:', error);
+        process.exit(1);
     }
+}
+
+if (!fs.existsSync(credsPath)) {
+    downloadSessionData();
 }
 
 async function start() {
@@ -75,7 +94,7 @@ async function start() {
         const Matrix = makeWASocket({
             version,
             logger: pino({ level: 'silent' }),
-            printQRInTerminal: useQR,
+            printQRInTerminal: true,
             browser: ["Ethix-MD", "safari", "3.3"],
             auth: state,
             getMessage: async (key) => {
@@ -90,7 +109,7 @@ async function start() {
         Matrix.ev.on('connection.update', (update) => {
             const { connection, lastDisconnect } = update;
             if (connection === 'close') {
-                if (lastDisconnect.error?.output?.statusCode !== DisconnectReason.loggedOut) {
+                if (lastDisconnect.error.output.statusCode !== DisconnectReason.loggedOut) {
                     start();
                 }
             } else if (connection === 'open') {
@@ -136,24 +155,7 @@ async function start() {
     }
 }
 
-async function init() {
-    if (fs.existsSync(credsPath)) {
-        console.log("🔒 Session file found, proceeding without QR code.");
-        await start();
-    } else {
-        const sessionDownloaded = await downloadSessionData();
-        if (sessionDownloaded) {
-            console.log("🔒 Session downloaded, starting bot.");
-            await start();
-        } else {
-            console.log("No session found or downloaded, QR code will be printed for authentication.");
-            useQR = true;
-            await start();
-        }
-    }
-}
-
-init();
+start();
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
