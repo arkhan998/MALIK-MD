79414dae92cc8297fdc421d8557ff3075e0e0eb2
Ata Ur Rehman Malik <119836227+arkhan998@users.noreply.github.com>
2025-05-13 06:52:53 +0500

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -18,8 +18,8 @@ const config = {
   REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || "public",
-  OWNER_NAME: process.env.OWNER_NAME || "🦋⃟ᴠͥɪͣᴘͫ✮⃝🇲ᗩしᏆᏦ 🇸αнαв𝄟⃝",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "923322964709",
+  OWNER_NAME: process.env.OWNER_NAME || "",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "",
   GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCUPaxfIdZawsKZKqCqJcC-GWiQPCXKTDc",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
 };
